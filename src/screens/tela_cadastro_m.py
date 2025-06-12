e62d6ac29e37ecb2d6ccc62272bfc811ebb447d1
Lucas-Beni <lucgarcbeni@gmail.com>
2025-06-11 22:40:13 -0300

    multiplos autores funcionando

--- src/screens/tela_cadastro_m.py
+++ src/screens/tela_cadastro_m.py
@@ -11,10 +11,24 @@ class TelaCadastroM(ft.Container):
             height=50
         )
 
+        self.autores = []
+
         self.autor = ft.TextField(
             label="Digite o nome do autor da música",
             width=300,
             height=50
+        )
+
+        self.autores.append(self.autor)
+
+        self.novo_autor = ft.ElevatedButton(
+            text="Adicionar novo autor",
+            on_click=self.criar_input
+        )
+
+        self.tirar_input = ft.ElevatedButton(
+            text="Tirar um autor",
+            on_click=self.remover_input
         )
 
         self.genero = ft.TextField(
@@ -30,10 +44,30 @@ class TelaCadastroM(ft.Container):
 
         self.content = (
             ft.Column(
-                controls=[self.nome_m,self.autor,self.genero,self.confirmar]
+                controls=[
+                    self.nome_m,self.autor,self.novo_autor,self.tirar_input,self.genero,self.confirmar
+                    ]
             )
         )
-    
+
+    def criar_input(self, e):
+        novo_input = ft.TextField(
+            label="Digite o nome do autor da música",
+            width=300,
+            height=50
+        )
+
+        self.autores.append(novo_input)
+        self.content.controls.insert(-4, novo_input)
+        self.update()
+        print(self.autores)
+
+    def remover_input(self, e):
+        if len(self.content.controls) > 6:  # Impede remover se houver só 1 autor
+            del self.content.controls[-5]
+            self.autores.pop()
+            self.update()
+        print(self.autores)
     def salvar(self, e):
         try:
             conn = sql.connect('urna.db')
@@ -70,10 +104,17 @@ class TelaCadastroM(ft.Container):
 
                 conn.commit()
 
-                cursor.execute('''
-                    INSERT INTO dimAutores(nome_autor)
-                    VALUES(?)
-                ''', (self.autor.value.lower(),))
+                for autor in self.autores:
+                    cursor.execute('''
+                        SELECT * FROM dimAutores WHERE nome_autor = ?
+                    ''', (autor.value.lower(),))
+                    autor_existe = cursor.fetchone()
+
+                    if not autor_existe:
+                        cursor.execute('''
+                            INSERT INTO dimAutores(nome_autor)
+                            VALUES(?)
+                        ''', (autor.value.lower(),))
 
                 conn.commit()
                 conn.close()
